b87c6f588d6818c3e6af1e67ef053294ec8ca261
bit03class <bit01class@gmail.com>
2019-08-01 10:26:17 +0900

    init

---
+++ sts08/src/main/java/com/bit/day07/HomeController.java
@@ -0,0 +1,22 @@
+package com.bit.day07;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home() {
+		logger.debug("ÀÎµ¦½º ·Îµù...");
+		
+		
+		return "home";
+	}
+	
+}
---
+++ sts08/src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=utf-8" 
+	pageEncoding="utf-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.css" />
+	<script type="text/javascript" src="js/jquery-1.12.4.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+</head>
+<body>
+
+</body>
+</html>
